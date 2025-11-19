36a477749ac75cffa32f3c7387992f28ce89f4be
venkateswarlu.dande <venkateswarlu.dande@centific.com>
2025-11-19 18:20:55 +0530

    implemented mongoose

--- controllers/products-controllers.js
+++ controllers/products-controllers.js
@@ -4,47 +4,69 @@ const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
 const HttpError = require('../models/http-error');
-
+const mongoose = require('mongoose');
 const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;   
 
-const getProducts = async (req, res, next) => {
-    const client = new MongoClient(url);
-    let products;   
+const Product = require('../models/products'); 
 
-    try {
-        await client.connect();
-        const db = client.db();
-        const productsCollection = db.collection('products');
-        products = await productsCollection.find().toArray();
-    } catch (err) {
-        const error = new HttpError('Fetching products failed, please try again later.', 500);
-        return next(error);
-    }
+mongoose.connect(url)
+    .then(() => {
+        console.log('Connected to MongoDB');        
+    })
+    .catch(err => {
+        console.log('Failed to connect to MongoDB', err);
+    });
 
-    client.close();
-    res.status(200).json({ products: products });
-};
+
 
 const createProduct = async (req, res, next) => {
-    const newProduct = {
+    const createdProduct = new Product({
         name: req.body.name,
         price: req.body.price
-    }; 
-    const client = new MongoClient(url);
+    }); 
 
-    try {
-        await client.connect();
-        const db = client.db();
-        const productsCollection = db.collection('products');
-        const result = await productsCollection.insertOne(newProduct);
-        res.status(201).json({ product: newProduct });
-    } catch (err) {
-        const error = new HttpError('Creating product failed, please try again.', 500);
-        return next(error);
-    }
+    const result = await createdProduct.save();
 
-    client.close();
-    res.status(201).json({ product: newProduct });
+    res.status(201).json({ product: result });
+
+    //old code using MongoClient
+    //const client = new MongoClient(url);
+    // try {
+    //     await client.connect();
+    //     const db = client.db();
+    //     const productsCollection = db.collection('products');
+    //     const result = await productsCollection.insertOne(newProduct);
+    //     res.status(201).json({ product: newProduct });
+    // } catch (err) {
+    //     const error = new HttpError('Creating product failed, please try again.', 500);
+    //     return next(error);
+    // }
+
+    // client.close();
+    // res.status(201).json({ product: newProduct });
+};
+
+const getProducts = async (req, res, next) => {
+
+    const products = await Product.find().exec();
+    res.status(200).json({ products: products });
+
+    //old code using MongoClient
+    // const client = new MongoClient(url);
+    // let products;   
+
+    // try {
+    //     await client.connect();
+    //     const db = client.db();
+    //     const productsCollection = db.collection('products');
+    //     products = await productsCollection.find().toArray();
+    // } catch (err) {
+    //     const error = new HttpError('Fetching products failed, please try again later.', 500);
+    //     return next(error);
+    // }
+
+    // client.close();
+    // res.status(200).json({ products: products });
 };
 
 
